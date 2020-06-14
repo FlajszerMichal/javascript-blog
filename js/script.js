21a94bff590861a9de3da19e35f76599cc24e7cf
Michał Flajszer <flajszermichal@hotmail.com>
2020-06-14 12:36:32 +0200

    eslint

--- js/script.js
+++ js/script.js
@@ -49,4 +49,51 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+
+  
 }
+
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+/* remove contents of titleList */
+
+function clearMessages(){
+	document.getElementById('messages').insertAdjacentHTML = 'titleList';
+}
+
+/* for each article */
+
+const articles = document.querySelectorAll('optArticleSelector');
+
+let html = '';
+
+for(let article of articles){
+  article.addEventListener('click', generateTitleLinks);
+}
+  /* get the article id */
+
+  const articleId = article.getAttribute('id');
+
+  /* find the title element */
+
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+  /* get the title from the title element */
+
+  /* create HTML of the link */
+
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  console.log('to jest poprawny kod HTML');
+
+  /* insert link into titleList */
+
+  titleList.insertAdjacentHTML = titleList.insertAdjacentHTML + linkHTML;
+
+}
+
+generateTitleLinks();
+  
